59ccc2323109af09f11d241f93ef653ef741c4db
haywah27 <hayleywahlroos15@gmail.com>
2021-04-01 14:02:49 -0700

    populating table with api fetch call

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React from "react";
-import API from "../src/utils/API"
-import { Table } from 'react-bootstrap'
-// import {Table} from 'react-bootstrap/Table'
-function App () {
-    console.log(API)
-    return (
-        
-        <>
-            <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        <td>First Name</td>
-                        <td>Last Name</td>
-                        <td>DOB</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Hay</td>
-                        <td>Wah</td>
-                        <td>02/15/2020</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </>
-    )
-}
+import React, { useEffect, useState  } from 'react';
+import EmployeeTable from "../src/EmployeeTable"
 
+function App() {
+  
+  return (
+    <div>
+      <EmployeeTable />
+    </div>
+  );
+};
+  
 export default App;
 
