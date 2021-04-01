05b385d666df638c060532217a28cbb13904ce35
haywah27 <hayleywahlroos15@gmail.com>
2021-03-31 21:28:37 -0700

    inital table structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import API from "../src/utils/API"
+import { Table } from 'react-bootstrap'
 function App () {
     console.log(API)
     return (
         
         <>
-            hello world
+            <Table>
+                <thead>
+                    <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>DOB</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hay</td>
+                        <td>Wah</td>
+                        <td>02/15/2020</td>
+                    </tr>
+                </tbody>
+            </Table>
         </>
     )
 }
